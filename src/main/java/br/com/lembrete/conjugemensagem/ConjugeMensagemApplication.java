27532dfaa8d152c723ccb7b3210c7a12b61d137d
unknown <zelto@hotmail.com>
2023-07-26 18:02:34 -0300

    inicia o projeto

---
+++ src/main/java/br/com/lembrete/conjugemensagem/ConjugeMensagemApplication.java
@@ -0,0 +1,13 @@
+package br.com.lembrete.conjugemensagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConjugeMensagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConjugeMensagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lembrete/conjugemensagem/ConjugeMensagemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lembrete.conjugemensagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConjugeMensagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
