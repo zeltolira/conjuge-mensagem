dfcfdbb04738fdf1b7016d01670e5c59c0fe9b51
unknown <zelto@hotmail.com>
2023-07-27 18:23:21 -0300

    fluxo do Repository. Teste OK

---
+++ src/main/java/br/com/lembrete/conjugemensagem/usuario/application/infra/UsuarioInfraRepositoy.java
@@ -0,0 +1,23 @@
+package br.com.lembrete.conjugemensagem.usuario.application.infra;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.lembrete.conjugemensagem.usuario.application.repository.UsuarioRepository;
+import br.com.lembrete.conjugemensagem.usuario.domain.Usuario;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Repository
+@Log4j2
+@RequiredArgsConstructor
+public class UsuarioInfraRepositoy implements UsuarioRepository {
+	private final UsuarioSpringDataJPARepository usuarioSpringDataJPARepository;
+
+	@Override
+	public Usuario salva(Usuario usuario) {
+	log.info("[inicia] UsuarioInfraRepositoy - salva");
+	usuarioSpringDataJPARepository.save(usuario);
+	log.info("[finaliza] UsuarioInfraRepositoy - salva");
+	return usuario;
+	}
+}
