3e3332a6b3595b71677b526b70bfe237a3351db0
unknown <zelto@hotmail.com>
2023-07-27 18:22:01 -0300

    implementa usuarioRepository

---
+++ src/main/java/br/com/lembrete/conjugemensagem/usuario/application/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.com.lembrete.conjugemensagem.usuario.application.repository;
+
+import br.com.lembrete.conjugemensagem.usuario.domain.Usuario;
+
+public interface UsuarioRepository {
+	Usuario salva(Usuario usuario);
+}
