f1eda5064e8518e057a78ca2c0ea5b09442f1327
unknown <zelto@hotmail.com>
2023-07-27 18:20:00 -0300

    impelmenta resquest e response

--- src/main/java/br/com/lembrete/conjugemensagem/usuario/application/api/request/UsuarioRequest.java
+++ src/main/java/br/com/lembrete/conjugemensagem/usuario/application/api/request/UsuarioRequest.java
@@ -1,5 +1,20 @@
 package br.com.lembrete.conjugemensagem.usuario.application.api.request;
 
-public class UsuarioRequest {
+import java.time.LocalDate;
 
+import br.com.lembrete.conjugemensagem.usuario.domain.Sexo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class UsuarioRequest {
+	@NotBlank
+	private String nomeUsuario;
+	@NotNull
+	private Sexo sexo;
+	@NotBlank
+	private String celular;
+	@NotNull
+	private LocalDate dataNascimento;
 }
--- src/main/java/br/com/lembrete/conjugemensagem/usuario/application/api/response/UsuarioResponse.java
+++ src/main/java/br/com/lembrete/conjugemensagem/usuario/application/api/response/UsuarioResponse.java
@@ -1,5 +1,12 @@
 package br.com.lembrete.conjugemensagem.usuario.application.api.response;
 
-public class UsuarioResponse {
+import java.util.UUID;
 
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class UsuarioResponse {
+	private UUID idUsuario;
 }
