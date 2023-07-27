204b43221d918dff07fe7a5cb0e920a1e111c7fc
unknown <zelto@hotmail.com>
2023-07-27 05:28:31 -0300

    implementa api de teste

--- src/main/java/br/com/lembrete/conjugemensagem/ConjugeMensagemApplication.java
+++ src/main/java/br/com/lembrete/conjugemensagem/ConjugeMensagemApplication.java
@@ -2,10 +2,20 @@ package br.com.lembrete.conjugemensagem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ConjugeMensagemApplication {
 
+	@GetMapping
+	public String getHomeTest() {
+		return "Lembrete do Amor";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConjugeMensagemApplication.class, args);
 	}
