b0307424e434a5ab313b465c8b0fccbe23f42a45
unknown <zelto@hotmail.com>
2023-07-27 18:17:11 -0300

    implementa controller

--- src/main/java/br/com/lembrete/conjugemensagem/usuario/application/api/UsuarioController.java
+++ src/main/java/br/com/lembrete/conjugemensagem/usuario/application/api/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lembrete.conjugemensagem.usuario.application.api.request.UsuarioRequest;
 import br.com.lembrete.conjugemensagem.usuario.application.api.response.UsuarioResponse;
+import br.com.lembrete.conjugemensagem.usuario.application.service.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -13,11 +14,14 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class UsuarioController implements UsuarioAPI {
 
+	private final UsuarioService usuarioService;
+
 	@Override
-	public UsuarioResponse postConjuge(@Valid UsuarioRequest conjugeRequeste) {
+	public UsuarioResponse postConjuge(@Valid UsuarioRequest usuarioRequest) {
 		log.info("[inicia] UsuarioController - postConjuge");
+		UsuarioResponse usuarioCriado = usuarioService.criaConjuge(usuarioRequest);
 		log.info("[finaliza] UsuarioController - postConjuge");
-		return null;
+		return usuarioCriado;
 	}
 
 }
