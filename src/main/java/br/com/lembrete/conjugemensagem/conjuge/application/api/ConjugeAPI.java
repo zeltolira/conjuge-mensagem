8340a6ff3fa205e865cc03309fe4e48a0376013e
unknown <zelto@hotmail.com>
2023-08-01 06:35:18 -0300

    implementa contrato da api-conjuge

---
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/api/ConjugeAPI.java
@@ -0,0 +1,25 @@
+package br.com.lembrete.conjugemensagem.conjuge.application.api;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lembrete.conjugemensagem.conjuge.application.request.ConjugeRequest;
+import br.com.lembrete.conjugemensagem.conjuge.application.response.ConjugeResponse;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/v1/conjuge")
+public interface ConjugeAPI {
+
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	ConjugeResponse postConjuge(@PathVariable UUID idUsuario, @Valid @RequestBody ConjugeRequest conjugeRequest);
+	
+}
