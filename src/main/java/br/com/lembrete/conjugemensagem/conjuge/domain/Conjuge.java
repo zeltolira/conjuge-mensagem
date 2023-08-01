d5121cbb80429070472602b9ed4b5d2d57edd4cd
unknown <zelto@hotmail.com>
2023-08-01 06:32:45 -0300

    implementa dominio de negocio 'conjuge'

---
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/domain/Conjuge.java
@@ -0,0 +1,48 @@
+package br.com.lembrete.conjugemensagem.conjuge.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import br.com.lembrete.conjugemensagem.usuario.domain.Sexo;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "conjuge")
+public class Conjuge {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idConjuge;
+	@NotBlank
+	private String nomeConjuge;
+	@NotNull
+	private Sexo sexo;
+	@NotBlank
+	private String celular;
+	@NotNull
+	private LocalDate dataNascimento;
+
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraUltimaAlteracao;
+	
+//	@OneToOne(mappedBy = "conjuge")
+//	private Usuario usuario;
+}
