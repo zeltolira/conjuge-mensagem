1c6f974aa6b66cf42f301f9905837c4dba886c62
unknown <zelto@hotmail.com>
2023-08-01 06:36:28 -0300

    implementa response e request

---
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/request/ConjugeRequest.java
@@ -0,0 +1,5 @@
+package br.com.lembrete.conjugemensagem.conjuge.application.request;
+
+public class ConjugeRequest {
+
+}
---
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/response/ConjugeResponse.java
@@ -0,0 +1,5 @@
+package br.com.lembrete.conjugemensagem.conjuge.application.response;
+
+public class ConjugeResponse {
+
+}
