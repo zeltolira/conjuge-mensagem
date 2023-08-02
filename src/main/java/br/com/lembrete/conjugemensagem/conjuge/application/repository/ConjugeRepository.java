28062d8dad13dd89babdca0d1140d0c81c13de46
unknown <zelto@hotmail.com>
2023-08-02 06:23:55 -0300

    configura repository

---
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/repository/ConjugeRepository.java
@@ -0,0 +1,7 @@
+package br.com.lembrete.conjugemensagem.conjuge.application.repository;
+
+import br.com.lembrete.conjugemensagem.conjuge.domain.Conjuge;
+
+public interface ConjugeRepository {
+	Conjuge salvaConjuge(Conjuge conjuge);
+}
