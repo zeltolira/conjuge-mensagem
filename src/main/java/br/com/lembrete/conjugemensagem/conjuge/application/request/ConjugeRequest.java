35ec6c1e1953c539dee6bad2323026fe8988624a
unknown <zelto@hotmail.com>
2023-08-02 06:17:17 -0300

    implementa response e request do conjuge

--- src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/request/ConjugeRequest.java
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/request/ConjugeRequest.java
@@ -1,5 +1,23 @@
 package br.com.lembrete.conjugemensagem.conjuge.application.request;
 
-public class ConjugeRequest {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import br.com.lembrete.conjugemensagem.usuario.domain.Sexo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class ConjugeRequest {
+	@NotBlank
+	private String nomeConjuge;
+	@NotNull
+	private Sexo sexo;
+	@NotBlank
+	private String celular;
+	@NotNull
+	private LocalDate dataNascimento;
+
+	private LocalDateTime dataHoraDoCadastro;
 }
--- src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/response/ConjugeResponse.java
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/response/ConjugeResponse.java
@@ -1,5 +1,12 @@
 package br.com.lembrete.conjugemensagem.conjuge.application.response;
 
-public class ConjugeResponse {
+import java.util.UUID;
 
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class ConjugeResponse {
+	private UUID idConjuge;
 }
