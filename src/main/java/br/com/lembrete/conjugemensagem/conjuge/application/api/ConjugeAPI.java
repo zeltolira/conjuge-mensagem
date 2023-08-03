dbd2995be0fe5a903107791a6bd4764037a25b45
unknown <zelto@hotmail.com>
2023-08-03 05:53:16 -0300

    implementa camada de api

--- src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/api/ConjugeAPI.java
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/api/ConjugeAPI.java
@@ -1,8 +1,10 @@
 package br.com.lembrete.conjugemensagem.conjuge.application.api;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lembrete.conjugemensagem.conjuge.application.api.request.ConjugeRequest;
+import br.com.lembrete.conjugemensagem.conjuge.application.api.response.ConjugeListResponse;
 import br.com.lembrete.conjugemensagem.conjuge.application.api.response.ConjugeResponse;
 import jakarta.validation.Valid;
 
@@ -21,5 +24,9 @@ public interface ConjugeAPI {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	ConjugeResponse postConjuge(@PathVariable UUID idUsuario, @Valid @RequestBody ConjugeRequest conjugeRequest);
+	
+	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
+	List<ConjugeListResponse> getTodosConjuges();
 	
 }
