f16d7b38512813334498121007cb2e8614aaeb50
unknown <zelto@hotmail.com>
2023-08-03 06:27:07 -0300

    implementa ConjugeListResponse

--- src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/api/response/ConjugeListResponse.java
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/api/response/ConjugeListResponse.java
@@ -1,8 +1,11 @@
 package br.com.lembrete.conjugemensagem.conjuge.application.api.response;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import br.com.lembrete.conjugemensagem.conjuge.domain.Conjuge;
 import br.com.lembrete.conjugemensagem.usuario.domain.Sexo;
 import lombok.Value;
 
@@ -14,4 +17,19 @@ public class ConjugeListResponse {
 	private String celular;
 	private LocalDate dataNascimento;
 
+	public static List<ConjugeListResponse> converte(List<Conjuge> conjuges) {
+		return conjuges.stream()
+				.map(ConjugeListResponse::new)
+				.collect(Collectors.toList());
+	}
+
+	public ConjugeListResponse(Conjuge conjuge) {
+		this.idConjuge = conjuge.getIdConjuge();
+		this.nomeConjuge = conjuge.getNomeConjuge();
+		this.sexo = conjuge.getSexo();
+		this.celular = conjuge.getCelular();
+		this.dataNascimento = conjuge.getDataNascimento();
+	}
+	
+	
 }
