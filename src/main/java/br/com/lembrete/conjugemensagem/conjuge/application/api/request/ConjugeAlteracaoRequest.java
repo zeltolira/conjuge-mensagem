4d435b44e48b699eeaa183b8dc18d8e64cde8f2b
unknown <zelto@hotmail.com>
2023-08-04 05:52:16 -0300

    implementa ConjugeAlteracaoRequest

---
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/api/request/ConjugeAlteracaoRequest.java
@@ -0,0 +1,24 @@
+package br.com.lembrete.conjugemensagem.conjuge.application.api.request;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import br.com.lembrete.conjugemensagem.usuario.domain.Sexo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class ConjugeAlteracaoRequest {
+	@NotBlank
+	private String nomeConjuge;
+	@NotNull
+	private Sexo sexo;
+	@NotBlank
+	private String celular;
+	@NotNull
+	private LocalDate dataNascimento;
+
+	private LocalDateTime dataHoraUltimaAlteracao;
+
+}
