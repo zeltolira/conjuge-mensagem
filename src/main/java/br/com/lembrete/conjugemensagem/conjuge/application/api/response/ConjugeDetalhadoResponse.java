34602635c7f9f79f13feb1d01e7eaad7a8fcc050
unknown <zelto@hotmail.com>
2023-08-04 05:43:47 -0300

    implementa ConjugeDetlhadoResponse

--- src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/api/response/ConjugeDetalhadoResponse.java
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/api/response/ConjugeDetalhadoResponse.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import br.com.lembrete.conjugemensagem.conjuge.domain.Conjuge;
 import br.com.lembrete.conjugemensagem.usuario.domain.Sexo;
 import lombok.Value;
 
@@ -15,4 +16,13 @@ public class ConjugeDetalhadoResponse {
 	private String celular;
 	private LocalDate dataNascimento;
 	private LocalDateTime dataHoraUltimaAlteracao;
+	
+	public ConjugeDetalhadoResponse(Conjuge conjuge) {
+		this.idConjuge = conjuge.getIdConjuge();
+		this.nomeConjuge = conjuge.getNomeConjuge();
+		this.sexo = conjuge.getSexo();
+		this.celular = conjuge.getCelular();
+		this.dataNascimento = conjuge.getDataNascimento();
+		this.dataHoraUltimaAlteracao = LocalDateTime.now();
+	}
 }
