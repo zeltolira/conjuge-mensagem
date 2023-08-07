cd10396d3dc8335d326d817f58b7c801e07c9e95
unknown <zelto@hotmail.com>
2023-08-07 14:27:17 -0300

    implementa domain mensagem

---
+++ src/main/java/br/com/lembrete/conjugemensagem/mensagem/domain/Mensagem.java
@@ -0,0 +1,36 @@
+package br.com.lembrete.conjugemensagem.mensagem.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "mensagem")
+public class Mensagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idMensagem;
+	@NotBlank
+	private String mensagem;
+	
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraUltimaAlteracao;
+}
