4f38fc30d9eb70c24b3753a17556b3d9fc9d59ba
unknown <zelto@hotmail.com>
2023-08-08 05:04:12 -0300

    incrementa postMensagem na camada de api

--- src/main/java/br/com/lembrete/conjugemensagem/mensagem/application/api/MensagemAPI.java
+++ src/main/java/br/com/lembrete/conjugemensagem/mensagem/application/api/MensagemAPI.java
@@ -5,18 +5,20 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lembrete.conjugemensagem.mensagem.application.api.request.MensagemRequest;
 import br.com.lembrete.conjugemensagem.mensagem.application.api.response.MensagemResponse;
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping(value = "/v1/usuario/{idUsuario}/conjuge{idConjuge}/mensagem")
+@RequestMapping(value = "/v1/usuario/{idUsuario}/conjuge/{idConjuge}/mensagem")
 public interface MensagemAPI {
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	MensagemResponse postMensagem(@PathVariable UUID idConjuge, MensagemRequest mensagemRequest);
+	MensagemResponse postMensagem(@PathVariable UUID idConjuge,@Valid @RequestBody MensagemRequest mensagemRequest);
 }
