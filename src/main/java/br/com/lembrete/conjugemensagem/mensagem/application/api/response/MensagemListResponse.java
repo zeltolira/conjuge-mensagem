56bcb063b2cb9057de3eae1aa4ff7375d9aa5ff4
unknown <zelto@hotmail.com>
2023-08-08 05:44:30 -0300

    implementa listResponse

--- src/main/java/br/com/lembrete/conjugemensagem/mensagem/application/api/response/MensagemListResponse.java
+++ src/main/java/br/com/lembrete/conjugemensagem/mensagem/application/api/response/MensagemListResponse.java
@@ -1,12 +1,29 @@
 package br.com.lembrete.conjugemensagem.mensagem.application.api.response;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import br.com.lembrete.conjugemensagem.mensagem.domain.Mensagem;
 import lombok.Value;
 
 @Value
 public class MensagemListResponse {
 	private UUID idMensagem;
 	private String mensagemAReceber;
+	
+	
+	public static List<MensagemListResponse> converte(List<Mensagem> mensagens) {
+		return mensagens.stream()
+				.map(MensagemListResponse::new)
+				.collect(Collectors.toList());
+	}
+
+
+	public MensagemListResponse(Mensagem mensagens) {
+		this.idMensagem = mensagens.getIdMensagem();
+		this.mensagemAReceber = mensagens.getMensagemAReceber();
+	}
+	
 	
 }
