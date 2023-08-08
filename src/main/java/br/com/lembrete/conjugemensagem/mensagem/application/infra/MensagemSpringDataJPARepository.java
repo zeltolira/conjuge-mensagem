ae17f8ae816b614edb486c3be2c6e334f0731406
unknown <zelto@hotmail.com>
2023-08-08 05:17:09 -0300

    implementa camada MensagemSpringDataJPARepository

---
+++ src/main/java/br/com/lembrete/conjugemensagem/mensagem/application/infra/MensagemSpringDataJPARepository.java
@@ -0,0 +1,11 @@
+package br.com.lembrete.conjugemensagem.mensagem.application.infra;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.lembrete.conjugemensagem.mensagem.domain.Mensagem;
+
+public interface MensagemSpringDataJPARepository extends JpaRepository<Mensagem, UUID>{
+
+}
