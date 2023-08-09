5d52cef964594a45c6a6efbae41789f66271e256
unknown <zelto@hotmail.com>
2023-08-09 18:07:30 -0300

    implementa delete no mensagemApi

--- src/main/java/br/com/lembrete/conjugemensagem/mensagem/application/api/MensagemAPI.java
+++ src/main/java/br/com/lembrete/conjugemensagem/mensagem/application/api/MensagemAPI.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,4 +40,8 @@ public interface MensagemAPI {
 	@PatchMapping(value = "/{idMensagem}")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	void PatchMensagem(@PathVariable UUID idConjuge, @PathVariable UUID idMensagem, @Valid @RequestBody MensagemAlteracaoRequest mensagemAlteracaoRequest);
+	
+	@DeleteMapping(value = "/{idMensagem}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	void deletaMensagemPorId(@PathVariable UUID idConjuge, @PathVariable UUID idMensagem);
 }
