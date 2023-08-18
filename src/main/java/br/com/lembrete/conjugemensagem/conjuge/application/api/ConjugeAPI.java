240e4b49bd21673d876d7a67829fe096aa40c644
unknown <zelto@hotmail.com>
2023-08-18 13:11:41 -0300

    altera contrato da Api

--- src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/api/ConjugeAPI.java
+++ src/main/java/br/com/lembrete/conjugemensagem/conjuge/application/api/ConjugeAPI.java
@@ -4,15 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.lembrete.conjugemensagem.conjuge.application.api.request.ConjugeAlteracaoRequest;
 import br.com.lembrete.conjugemensagem.conjuge.application.api.request.ConjugeRequest;
@@ -21,13 +13,12 @@ import br.com.lembrete.conjugemensagem.conjuge.application.api.response.ConjugeL
 import br.com.lembrete.conjugemensagem.conjuge.application.api.response.ConjugeResponse;
 import jakarta.validation.Valid;
 
-@RestController
 @RequestMapping(value = "/v1/conjuge/{idUsuario}")
 public interface ConjugeAPI {
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	ConjugeResponse postConjuge(@PathVariable UUID idUsuario, @Valid @RequestBody ConjugeRequest conjugeRequest);
+	ConjugeResponse postConjuge(@RequestParam UUID idUsuario, @Valid @RequestBody ConjugeRequest conjugeRequest);
 	
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.OK)
